a0acfe3795be3f5c6008d1b1b35b441f4f52577d
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-06-21 08:48:35 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int h,m;
+
+    cin >> h >> m;
+
+	m -= 45;
+	if (m < 0) {
+		m += 60;
+		h -= 1;
+
+		if (h < 0) h = 23;
+	}
+
+    cout << h << " "<<  m;
+
+}
