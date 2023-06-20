d3c5594f7421c8c32a41f81aaac7c0637e3e45d9
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-06-21 01:02:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+	string str;
+	
+	getline(cin, str);
+
+	int cnt = 1;
+
+	bool null = false;
+
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ') {
+			cnt++;
+		}
+	}
+
+	if (str[0] == ' ') {
+		cnt--;
+	}
+	if (str[str.length()-1] == ' ') {
+		cnt--;
+	}
+
+	cout << cnt;
+}
