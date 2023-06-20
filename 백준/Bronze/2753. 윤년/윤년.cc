5ca2d4b194cdf1952d453b05fa11288fb60bf1e4
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-06-21 08:43:33 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    if(n % 4 == 0){
+        if(n%100 != 0 || n%400 ==0){
+            cout << 1 << endl;
+        }
+        else{
+        cout << 0<< endl;
+        }
+    }
+    else{
+        cout << 0<< endl;
+    }
+}
