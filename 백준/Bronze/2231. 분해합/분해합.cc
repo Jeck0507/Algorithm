0112975a09b69a7ff5312c96a9de07c42ebc5b5a
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-06-23 13:52:07 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i < n; i++) {
+        int temp = i;
+        int num = i;
+
+        while (temp > 0) {
+            num += temp % 10;
+            temp /= 10;
+        }
+
+        if (num == n) {
+            cout << i;
+            n = 0;
+            break;
+        }
+    }
+    if (n != 0) {
+        cout << "0";
+    }
+}
