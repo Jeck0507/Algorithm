1eafb9ff78a7739848d7d35ad5c21fe43ed8728b
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-06-28 13:46:11 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int dp[1000001];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+    }
+
+    cout << dp[n] % 15746;
+}
