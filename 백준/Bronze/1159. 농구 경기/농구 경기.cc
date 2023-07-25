874c8c4a13b3dba2d044f3694c0768ba304bed92
JeongHyck <68423074+Jeck0507@users.noreply.github.com>
2023-07-25 20:15:03 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+using namespace std;
+int n, cnt[26];
+string s, ret;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        cnt[s[0] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i] >= 5) {
+            ret += i + 'a';
+        }
+    }
+    if (ret.size()) {
+        cout << ret << endl;
+    } else {
+        cout << "PREDAJA" << endl;
+    }
+}
